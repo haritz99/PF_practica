95a195bcacf5df4e31f5621ee81a6d153001bf65
Haritz <haritzgomezsarasola04@gmail.com>
2025-12-03 19:18:57 +0100

    pokoyo

--- ioparsing.hs
+++ ioparsing.hs
@@ -1,3 +1,11 @@
 module IOPARSING where
 import Tipos (Serie)
+import Principales (printSerie, misSeries)
 import Data.ByteString (toFilePath)
+
+
+imprimirCatalogoConsola:: [Serie] -> IO ()
+imprimirCatalogoConsola xs = do 
+    putStrLn "Catálogo de Series: "
+    mapM_ (\(i, s) -> putStr (show i ++ ". " ++ printSerie s)) (zip [1..] xs)
+    
